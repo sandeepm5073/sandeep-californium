f88ff0206533dc0349bf143eb419389d6d6ad073
sandeepm5073 <66156339+sandeepm5073@users.noreply.github.com>
2022-12-14 22:50:30 +0530

    path vs query

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,47 +7,47 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
-router.get("/random" , function(req, res) {
+router.get("/random", function(req, res) {
     res.send("hi there")
 })
 
 
-router.get("/test-api" , function(req, res) {
+router.get("/test-api", function(req, res) {
     res.send("hi FunctionUp")
 })
 
 
-router.get("/test-api-2" , function(req, res) {
+router.get("/test-api-2", function(req, res) {
     res.send("hi FunctionUp. This is another cool API")
 })
 
 
-router.get("/test-api-3" , function(req, res) {
+router.get("/test-api-3", function(req, res) {
     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
 })
 
 
-router.get("/test-api-4" , function(req, res) {
+router.get("/test-api-4", function(req, res) {
     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
 })
 
 
 
-router.get("/test-api-5" , function(req, res) {
+router.get("/test-api-5", function(req, res) {
     res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
 })
 
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
+router.get("/test-api-6", function(req, res) {
+    res.send({ a: 56, b: 45 })
 })
 
 router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
+    res.send([23, 45, 6])
 })
 
 
 router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
+    res.send({ msg: "hi", status: true })
 })
 
 router.post("/test-post-3", function(req, res) {
@@ -56,18 +56,18 @@ router.post("/test-post-3", function(req, res) {
 
     // console.log( id , pwd)
 
-    console.log( req.body )
+    console.log(req.body)
 
-    res.send(  { msg: "hi" , status: true }  )
+    res.send({ msg: "hi", status: true })
 })
 
 
 
 router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
+    let arr = [12, "functionup"]
+    let ele = req.body.element
     arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
+    res.send({ msg: arr, status: true })
 })
 
 
@@ -79,10 +79,10 @@ router.post("/test-post-4", function(req, res) {
 
 
 router.get("/wiki/:countryName", function(req, res) {
-    
+
     // ...
     // hey nodejs , go and get me the data for countryName
-    res.send( {data: "something data"} )
+    res.send({ data: "something data" })
 })
 
 
@@ -107,10 +107,10 @@ router.get("/wiki/:countryName", function(req, res) {
 
 router.get("/:productName/p/:itemId", function(req, res) {
     let product = req.params.productName
-    let itemId= req.params.itemId
-    // ...
-    // hey nodejs, go and get me the data for hte variable ( its value) product 
-    res.send( {data: product} )
+    let itemId = req.params.itemId
+        // ...
+        // hey nodejs, go and get me the data for hte variable ( its value) product 
+    res.send({ data: product })
 })
 
 // QUERY params:-
@@ -122,42 +122,81 @@ router.get("/:productName/p/:itemId", function(req, res) {
 // localhost:3000/get-query-1?myCoolVar=soemthing&xyz=functionUp
 router.get("/get-query-1", function(req, res) {
     let data = req.query
-    let var1= data.myCoolVar
-    let var2= data.xyz
+    let var1 = data.myCoolVar
+    let var2 = data.xyz
 
-    console.log( data)
+    console.log(data)
 
-    res.send( {data: data, status: true} )
+    res.send({ data: data, status: true })
 })
 
 // take marks in req.query in a variable named "marks" and send "pass" if marks>40 else send "fail"
-router.get( "/get-query-2", function (req, res){
-    let marks= req.query.marks
-    let result = marks >40 ? "pass" : "fail"
-    //ternary operator
-    res.send( {data: result , status: true})
+router.get("/get-query-2", function(req, res) {
+    let marks = req.query.marks
+    let result = marks > 40 ? "pass" : "fail"
+        //ternary operator
+    res.send({ data: result, status: true })
 })
 
 //query params ( as well as path params ) can be sent in post request as well
-router.post( "/post-query-1", function (req, res){
-    let data= req.query
-    console.log( data)
-    res.send( {data: data , status: true})
+router.post("/post-query-1", function(req, res) {
+    let data = req.query
+    console.log(data)
+    res.send({ data: data, status: true })
 })
 
 //filter out all the numbers that are greater than "input" ( input is received from query params)
 let myArr = [23, 45, 67, 281394, 32424, 423, 24, 42323, 4, 234, 12, 34]
 
-router.post( "/post-query-2", function (req, res){
-    let input= req.query.input
+router.post("/post-query-2", function(req, res) {
+    let input = req.query.input
 
     // let finArr= myArr.filter ( ele => ele>input )
-    let finalArr= []
-    for( i=0 ; i<myArr.length ; i++){
-        if ( myArr[i] > input )     finalArr.push( myArr[i])
+    let finalArr = []
+    for (i = 0; i < myArr.length; i++) {
+        if (myArr[i] > input) finalArr.push(myArr[i])
     }
-    res.send( {data: finalArr , status: true})
+    res.send({ data: finalArr, status: true })
 })
+
+let persons = [{
+        name: "PK",
+        age: 10,
+        votingStatus: false
+    },
+    {
+        name: "SK",
+        age: 20,
+        votingStatus: false
+    },
+    {
+        name: "AA",
+        age: 70,
+        votingStatus: false
+    },
+    {
+        name: "SC",
+        age: 5,
+        votingStatus: false
+    },
+    {
+        name: "HO",
+        age: 40,
+        votingStatus: false
+    }
+]
+router.post("/post-query-3", function(req, res) {
+    let voting = req.query.votingAge
+    for (i = 0; i < persons.length; i++)
+        if (persons[i].age > voting) {
+            persons[i].votingStatus = true
+        }
+
+    let a = persons.filter(x => x.votingStatus === true)
+    res.send({ data: a, status: true })
+})
+
+
 
 
 module.exports = router;
