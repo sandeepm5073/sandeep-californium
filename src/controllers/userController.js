c4af6210ccfb1bd6e711755e1f1817257c0d8f5b
sabiha-thorium <miamoscovitzs@gmail.com>
2022-12-26 15:03:50 +0530

    add assignment solution

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,60 +1,23 @@
-const { type } = require("express/lib/response")
-const UserModel= require("../models/userModel")
-
-
-
-
-const basicCode= async function(req, res) {
-    
-    let contentTypeHeader = req.headers.content-type
-    console.log("The headers received in this request are: ", req.headers)
-    console.log("The content type header is: ", contentTypeHeader)
-
-
-    req.headers.month = "December"
-    req.batch = "Californium"
-
-    console.log("The headers modified from this request are: ", req.headers)
-    // let tokenDataInHeaders= req.headers.token
-    // console.log(tokenDataInHeaders)
-    res.header("year", "2022")
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-
-    console.log("The request object looks like this: ", req)
-    res.send({ msg: "This is coming from controller (handler)"})
-    
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const userModel= require("../models/userModel")
 
 const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
+    let appHeader = req.headers["isFreeAppUser"]
+    if(!appHeader) appHeader = req.headers["isfreeappuser"]
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+    if(!appHeader) return res.send({status: false, message:"The mandatory header is not present"})
+
+    console.log("request header is", appHeader)
+    
+    let data= req.body
+
+    if(appHeader == 'true') {
+        data.isFreeAppUser = true
+    } else {
+        data.isFreeAppUser = false
+    }
+
+    let savedData= await userModel.create(data)
+    res.send({status: true, data: savedData})
 }
 
 module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
